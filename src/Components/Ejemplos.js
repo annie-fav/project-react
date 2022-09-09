e5610260f9bf7f6b4527f4caf847646e53e406b2
annie <fav.hala@gmail.com>
2022-09-09 18:09:55 -0300

    Category

--- src/Components/Ejemplos.js
+++ src/Components/Ejemplos.js
@@ -28,102 +28,128 @@ import React, { useState, useEffect } from 'React'
 // export default Usuarios
 
 
-const MiComponente = () => {
+// const MiComponente = () => {
 
-    const [contador, setContador] = useState(0);
-    const [numero, setNumero] = useState(0);
+//     const [contador, setContador] = useState(0);
+//     const [numero, setNumero] = useState(0);
 
-    useEffect( () => {
-        setNumero(numero + 2);
+//     useEffect( () => {
+//         setNumero(numero + 2);
 
         // setTimeout(() => {
         //     console.log("useEffect")
         // }, 2000)
 
-        const interval = setInterval(() => {
-            console.log(ping)
-        }, 2000)
+    //     const interval = setInterval(() => {
+    //         console.log(ping)
+    //     }, 2000)
 
-        return (() => {
-           clearInterval(interval)
-        })
+    //     return (() => {
+    //        clearInterval(interval)
+    //     })
 
-    }, [contador]);
+    // }, [contador]);
   
 
 
-    const sumar = () => {
-        setContador(contador + 1)
-    }
+    // const sumar = () => {
+    //     setContador(contador + 1)
+    // }
 
-    const restar = () => {
-        setContador(contador - 1)
-    }
+//     const restar = () => {
+//         setContador(contador - 1)
+//     }
 
-    const reset = () => {
-        setContador(0)
-    }
+//     const reset = () => {
+//         setContador(0)
+//     }
 
-    }
+//     }
 
-const Ejemplo = () => {
+// const Ejemplo = () => {
 
-const [show setShow] = useState(true)
+// const [show setShow] = useState(true)
 
-const alternar = () => {
-    setShow(!show)
-}
+// const alternar = () => {
+//     setShow(!show)
+// }
 
-return (
-   <>
-   {show ? <MiComponente/> : <h1>No hay nada</h1>}
-   <button onClick={alternar}>Alternar</button>
-   </>
-)
+// return (
+//    <>
+//    {show ? <MiComponente/> : <h1>No hay nada</h1>}
+//    <button onClick={alternar}>Alternar</button>
+//    </>
+// )
 
-}
-
-
-// Promesas
-
-const initialProducts = [
-    { name: "Manzanas", id:0, price:200 },
-    { name: "Bananas", id:1, price:210 },
-    { name: "Peras", id:2, price:220 },
-    { name: "Sandia", id:3, price:230 }
-]
-
-const Promesa = new Promise((res, rej) => {
-    res(initialProducts)
-})
-
-   const [products, setProducts] = useState([]);
+// }
 
 
-   useEffect(() => {
-    Promesa
-    .then((data) => {
-        console.log("esta todo bien");
-        console.log(data);
-        setProducts(data);
-    })
-    .catch(() => {
-        console.log("error")
-    })
-   }, []);
+// // Promesas
 
-     return (
+// const initialProducts = [
+//     { name: "Manzanas", id:0, price:200 },
+//     { name: "Bananas", id:1, price:210 },
+//     { name: "Peras", id:2, price:220 },
+//     { name: "Sandia", id:3, price:230 }
+// ]
 
-        <>
+// const Promesa = new Promise((res, rej) => {
+//     res(initialProducts)
+// })
+
+//    const [products, setProducts] = useState([]);
+
+
+//    useEffect(() => {
+//     Promesa
+//     .then((data) => {
+//         console.log("esta todo bien");
+//         console.log(data);
+//         setProducts(data);
+//     })
+//     .catch(() => {
+//         console.log("error")
+//     })
+//    }, []);
+
+//      return (
+
+//         <>
         
-        {products.map((product) => {
-            <span key={product.id}>{product.name}</span>
-        })}
+//         {products.map((product) => {
+//             <span key={product.id}>{product.name}</span>
+//         })}
         
-        </>
-     )
+//         </>
+//      )
 
 
      // Desafio
 
-     
+    //  const ItemCount = ( { initial, stock, onAdd } ) => {
+
+    //   const [count, setCount] = useState(initial)
+
+    //     let contador = 0;
+        
+    //     const decrese = () => {
+    //         contador = contador - 1
+    //     }
+
+    //     const decrease = () => count > initial && setCount(count - 1)
+    //     const increase = () => count < stock && setCount(count + 1)
+
+    //     return (
+    //         <>
+    //           <button disabled={count===initial} onClick={decrese}>-</button>
+    //           <p>{contador}</p>
+    //           <button disabled={count === stock} onClick={increase}>+</button>
+    //           <button>Add to Cart</button>
+    //         </>
+    //     )
+    //  }
+
+    //  export default ItemCount
+
+
+    
