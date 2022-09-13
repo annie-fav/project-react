b668ec0fe1bca629c931b75a66dc2dee89176d33
annie <fav.hala@gmail.com>
2022-09-13 10:17:34 -0300

    Playing

--- src/Components/Main/Item.jsx
+++ src/Components/Main/Item.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ItemCount from './ItemCount'
+import './ItemListContainer.css'
 
 const Item = (props) => {
     const { item } = props
+
+
+
+
     return (
         <div className="card" key={item.id} href=''>
             <img className="img-card" src={item.img} alt={item.id} />
             <p className="title">{item.title}</p>
             <p className="price">Price: {item.price}</p>
             <hr></hr>
-            <ItemCount stock="9" />
+            <button className="seeDetail" onClick="">See detail</button>
         </div>
     )
 }
